d80a2392cef10677bf0d7e12527893a2857e060e
nope <55818423+maxsspot@users.noreply.github.com>
2025-02-25 18:29:40 -0500

    Create firebaseConfig.js

---
+++ firebaseConfig.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-app.js";
+import { getDatabase, ref, set, get, remove, onValue, update, push } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-database.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAMHdGoXT1dPH1d0YqMVsTXwY583tbpvQU",
+  authDomain: "song-request-2dde2.firebaseapp.com",
+  projectId: "song-request-2dde2",
+  storageBucket: "song-request-2dde2.firebasestorage.app",
+  messagingSenderId: "1050997422155",
+  appId: "1:1050997422155:web:8f1a7b1e36d40ee56b75ce"
+};
+
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+
+export { database };
