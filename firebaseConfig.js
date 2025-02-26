fdecaf48fda7fe49dfbc45cd8786653af4c56cf7
nope <55818423+maxsspot@users.noreply.github.com>
2025-02-25 20:02:05 -0500

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -4,6 +4,7 @@ import { getDatabase, ref, set } from "https://www.gstatic.com/firebasejs/11.0.2
 const firebaseConfig = {
   apiKey: "AIzaSyAMHdGoXT1dPH1d0YqMVsTXwY583tbpvQU",
   authDomain: "song-request-2dde2.firebaseapp.com",
+  databaseURL: "https://song-request-2dde2-default-rtdb.firebaseio.com/",
   projectId: "song-request-2dde2",
   storageBucket: "song-request-2dde2.appspot.com",
   messagingSenderId: "1050997422155",
